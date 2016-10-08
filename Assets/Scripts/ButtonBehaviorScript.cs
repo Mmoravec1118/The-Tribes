4e703ad7c90addd98448147c7e590dc4c37c701d
Zer0Ph34r <sepheroth0504@yahoo.com>
2016-10-08 12:11:23 -0600

    Added three scenes and all the framework for the main menu.  It's not fully functional, but it will get you to where you need to go

---
+++ Assets/Scripts/ButtonBehaviorScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonBehaviorScript : MonoBehaviour {
+
+    public string sceneName;
+
+    public Button playbutton;
+    public Button rulesButton;
+    public int players = 1;
+
+    public void OnValueChange()
+    {
+        players = (int)GetComponent<Slider>().value;
+    }
+
+    public void ChangeScene()
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Play()
+    {
+        playbutton.gameObject.SetActive(true);
+    }
+
+    public void Rules()
+    {
+        rulesButton.gameObject.SetActive(true);
+    }
+
+    public void OnRuleButton ()
+    {
+        rulesButton.gameObject.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
