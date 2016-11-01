99c361a388f8a7094d54d3fa638fd1a58d109c3a
Mmoravec1118 <marty.kylar@gmail.com>
2016-11-01 16:00:52 -0600

    Card class
    Created card class structure

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,6 +2,20 @@
 using System.Collections;
 
 public class Card : MonoBehaviour {
+    public enum CardType { Event, GlobalEvent, Scar }
+    public enum Traits { Strength, Agility, Trust, Survival, Notoriety }
+    public enum Resources { Wood, Stone, People, Food }
+
+    string title { get; set; }
+    string description { get; set; }
+    CardChoice[] choices = new CardChoice[3];
+
+    public Card(string title, string description, CardChoice[] choices)
+    {
+        this.title = title;
+        this.description = description;
+        this.choices = choices;
+    }
 
 	// Use this for initialization
 	void Start () {
