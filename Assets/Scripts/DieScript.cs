b8e3634a6cf71bff9b80ca3ef9a09ae8fef6a47b
Zer0Ph34r <sepheroth0504@yahoo.com>
2016-11-02 11:44:59 -0600

    Created a random die generator with display and button

---
+++ Assets/Scripts/DieScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DieScript : MonoBehaviour {
+
+    int dieResult;
+
+    public Text dieDisplay;
+
+    bool dieRolling;
+    float rollingTimer = 0;
+
+    public void Update()
+    {
+        if (dieRolling)
+        {
+            rollingTimer += (Time.deltaTime);
+            InvokeRepeating("UpdateDisplay", 0, 0.9f);
+        }
+        if (rollingTimer >= 2)
+        {
+            rollingTimer = 0;
+            dieRolling = false;
+            CancelInvoke("UpdateDisplay");
+            dieResult = (int)Random.Range(1, 7);
+            dieDisplay.text = "" + dieResult;
+        }
+        
+    }
+
+    public void RollDie ()
+    {
+
+        dieRolling = true;
+        
+    }
+
+    public void UpdateDisplay ()
+    {
+        dieDisplay.text = "" + (int)Random.Range(1, 7);
+    }
+
+    public int DieResult
+    {
+        get
+        {
+            return dieResult;
+        }
+    }
+
+}
