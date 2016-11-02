c6c37fbbbb87e1beb472aa041b8d41bea747de10
dansyron <Syron@Dannys-MacBook-Air.local>
2016-11-02 14:59:20 -0600

    added deck script
    laladingding

--- Assets/Scripts/CardChoice.cs
+++ Assets/Scripts/CardChoice.cs
@@ -8,30 +8,41 @@ public class CardChoice : MonoBehaviour {
     Effect[] lossEffects;
     string description;
     string Description
+	string winText;
+	string lossText;
     {
         get
         {
             return description;
         }
     }
-	public CardChoice(string description, Card.Traits trait, int toBeat, Effect[] winEffects, Effect[] lossEffects)
+	public CardChoice(string description, string winText, string lossText, Card.Traits trait, int toBeat, Effect[] winEffects, Effect[] lossEffects)
     {
         this.description = description;
         this.winEffects = winEffects;
         this.lossEffects = lossEffects;
+		this.winText = winText;
+		this.lossText = lossText;
     }
 }
 
 public class Effect
 {
     int change;
-    Card.Traits toChange;
+    Card.Traits toChangeTrait;
+	Card.Resources toChangeResource;
 
-    public Effect(int change, Card.Traits toChange)
+    public Effect(int change, Card.Traits toChangeTrait)
     {
         this.change = change;
-        this.toChange = toChange;
+        this.toChangeTrait = toChangeTrait;
     }
+
+	public Effect(int change, Card.Resources toChangeResource)
+	{
+		this.change = change;
+		this.toChangeResource = toChangeResource;
+	}
 
     //public void MakeChange(Player player)
     //{
--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -17,33 +17,71 @@ public class Deck : MonoBehaviour {
 
 		//add items to the queue.
 		deck.Enqueue (
-			//new card
+			//new card one
 			new Card ("Orc Invasion",
 				"Your settlement has been invaded by orcs, they look like they want what you have.",
 
 				//choice 1
-				new CardChoice[] {new CardChoice ("STRENGTH - You run the orcs off with your military might(10).",
-				//win effect
-						new Effect[]{ },
+				new CardChoice[] {new CardChoice ("STRENGTH - You try to run the orcs off with your military might(10).",
+					"You successfully chase off the orcs",
+					"You fail to chase off the orcs",
+				//win effect,
+					Card.Traits.Strength,
+					1,
+					new Effect[]{
+						new Effect(1, Card.Resources.Wood),
+						new Effect(1, Card.Resources.Stone),
+						new Effect(1, Card.Resources.Food)
+					},
 					//lose effects
-						new Effect[]{ }),
+						new Effect[]{
+						new Effect(-1, Card.Resources.Wood),
+						new Effect(-1, Card.Resources.Stone),
+						new Effect(-1, Card.Resources.Food)}
+				),
 
 					//choice 2
 					new CardChoice ("AGILITY - Using stealth, you take down the orc leader(10)",
+						"The Orc Leader dies",
+						"The Orc Leader Thwards your assassination attempt",
+						Card.Traits.Agility,
+						10,
 						//win effect
-						new Effect[]{ },
+						new Effect[]{ 
+						new Effect(2, Card.Resources.People),
+						new Effect(1, Card.Traits.Strength)
+					},
 						//lose effect
-						new Effect{ }),
+					new Effect{-1, Card.Traits.Strength}
+					
+					),
 
 					//choice 3
-					new CardChoice ("NOTORIETY - Orcs are too afraid of you to attack, they offer a peace offering.(12)",
+					new CardChoice ("NOTORIETY - Maybe the Orcs are too afraid of you to attack and will offer a peace offering.(12)",
+						"They cower in fear at you!  They offer the peace offering",
+						"They laugh at you, taking your resources",
+						Card.Traits.Notoriety,
+						12,
 						//win effects
-						new Effect[]{ },
+						new Effect[]{
+						new Effect(1, Card.Resources.Wood),
+						new Effect(1, Card.Resources.Stone),
+						new Effect(1, Card.Resources.Food)	
+					},
 						//lose effect
-						new Effect[]{ })
+						new Effect[]{ 
+						new Effect(-1, Card.Traits.Notoriety),
+						new Effect(-1, Card.Resources.Stone),
+						new Effect(-1, Card.Resources.Wood),
+						new Effect(-1, Card.Resources.Food)
+					})
+				
+			
 				}
 			)
 		);
+
+		//add second card
 
 	}
 	
