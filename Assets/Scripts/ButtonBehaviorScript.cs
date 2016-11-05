2c590cabbaf37088ec4ac3a50ec1d4d9256620c3
Doug Jacob <djacob9001@gmail.com>
2016-11-05 04:30:34 -0600

    Reverted ButtonBehaviorScript to the version I'd been working on

--- Assets/Scripts/ButtonBehaviorScript.cs
+++ Assets/Scripts/ButtonBehaviorScript.cs
@@ -3,39 +3,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class ButtonBehaviorScript : MonoBehaviour {
+public class ButtonBehaviorScript : MonoBehaviour
+{
 
     // Buttons passed in
     public Button playbutton;
     public Button rulesButton;
     public Button playerSlideButton;
     public Text sliderText;
-<<<<<<< HEAD
-=======
-
-    int players = 1;
-
-    GlobalsScript globals;
-
-    public GlobalsScript.TribeType tribeType;
-
-    // Tribe Type Buttons
-    public Button okbutton;
-    public Button tribe0Flavor;
-    public Button tribe1Flavor;
-    public Button tribe2Flavor;
-    public Button tribe3Flavor;
-
-    // Tribe Type Text
-    public Text tribe0FlavorText;
-    public Text tribe1FlavorText;
-    public Text tribe2FlavorText;
-    public Text tribe3FlavorText;
-
-    public Image finalDecisionPanel;
-
-    // SLiders for stats
->>>>>>> 7e24bb51ae559c2879869d4f4797fddbd439940c
     public Slider playerCountSlider;
 
     int players = 2;
@@ -70,7 +45,7 @@ public class ButtonBehaviorScript : MonoBehaviour {
         rulesButton.gameObject.SetActive(true);
     }
 
-    public void OnRuleButton ()
+    public void OnRuleButton()
     {
         rulesButton.gameObject.SetActive(false);
     }
@@ -84,175 +59,4 @@ public class ButtonBehaviorScript : MonoBehaviour {
     {
         Application.Quit();
     }
-<<<<<<< HEAD
-=======
-
-    public void ActivateOK()
-    {
-        if (okbutton.gameObject.activeInHierarchy == false)
-        {
-            okbutton.gameObject.SetActive(true);
-        }
-    }
-
-    public void Tribe0Flavor()
-    {
-        tribeType = GlobalsScript.TribeType.Barbarian;
-
-        // This is where the selected value of the tribe is stored between scenes
-        PlayerPrefs.SetString(playerSelectionController.GetCurrentTribeTypeKey(), tribeType.ToString());
-
-        tribe0FlavorText.gameObject.SetActive(true);
-        tribe1FlavorText.gameObject.SetActive(false);
-        tribe2FlavorText.gameObject.SetActive(false);
-        tribe3FlavorText.gameObject.SetActive(false);
-
-        ActivateOK();
-    }
-
-    public void Tribe1Flavor()
-    {
-        tribeType = GlobalsScript.TribeType.PeaceMaker;
-
-        // This is where the selected value of the tribe is stored between scenes
-        PlayerPrefs.SetString(playerSelectionController.GetCurrentTribeTypeKey(), tribeType.ToString());
-
-        tribe0FlavorText.gameObject.SetActive(false);
-        tribe1FlavorText.gameObject.SetActive(true);
-        tribe2FlavorText.gameObject.SetActive(false);
-        tribe3FlavorText.gameObject.SetActive(false);
-
-        ActivateOK();
-    }
-
-    public void Tribe2Flavor()
-    {
-        tribeType = GlobalsScript.TribeType.Nomad;
-
-        // This is where the selected value of the tribe is stored between scenes
-        PlayerPrefs.SetString(playerSelectionController.GetCurrentTribeTypeKey(), tribeType.ToString());
-
-        tribe0FlavorText.gameObject.SetActive(false);
-        tribe1FlavorText.gameObject.SetActive(false);
-        tribe2FlavorText.gameObject.SetActive(true);
-        tribe3FlavorText.gameObject.SetActive(false);
-
-        ActivateOK();
-    }
-
-    public void Tribe3Flavor()
-    {
-        tribeType = GlobalsScript.TribeType.Farmer;
-
-        // This is where the selected value of the tribe is stored between scenes
-        PlayerPrefs.SetString(playerSelectionController.GetCurrentTribeTypeKey(), tribeType.ToString());
-
-        tribe0FlavorText.gameObject.SetActive(false);
-        tribe1FlavorText.gameObject.SetActive(false);
-        tribe2FlavorText.gameObject.SetActive(false);
-        tribe3FlavorText.gameObject.SetActive(true);
-
-        ActivateOK();
-    }
-
-    public void StrengthUpdate()
-    {
-        strengthStat = (int)strengthSlider.value;
-
-        // This is where the Strength stat is stored between scenes
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentStrengthStatKey(), strengthStat);
-    }
-
-    public void AgilityUpdate()
-    {
-        agilityStat = (int)agilitySlider.value;
-
-        // This is where the Agility stat is stored between scenes
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentAgilityStatKey(), agilityStat);
-    }
-
-    public void TrustUpdate()
-    {
-        trustStat = (int)trustSlider.value;
-
-        // This is where the Trust stat is stored between scenes
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentTrustStatKey(), trustStat);
-    }
-
-    public void SurvivalUpdate()
-    {
-        survivalStat = (int)survivalSlider.value;
-
-        // This is where the Survival stat is stored between scenes
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentSurvivalStatKey(), survivalStat);
-    }
-
-    public void NotorietyUpdate()
-    {
-        notorietyStat = (int)notorietySlider.value;
-
-        // This is where the Notoriety stat is stored between scenes
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentNotorietyStatKey(), notorietyStat);
-    }
-
-    public void OnOKClick()
-    {
-        tribe0Flavor.gameObject.SetActive(false);
-        tribe1Flavor.gameObject.SetActive(false);
-        tribe2Flavor.gameObject.SetActive(false);
-        tribe3Flavor.gameObject.SetActive(false);
-
-        strengthSlider.gameObject.SetActive(false);
-        agilitySlider.gameObject.SetActive(false);
-        trustSlider.gameObject.SetActive(false);
-        survivalSlider.gameObject.SetActive(false);
-        notorietySlider.gameObject.SetActive(false);
-
-        okbutton.gameObject.SetActive(false);
-
-        finalDecisionPanel.gameObject.SetActive(true);
-    }
-
-    public void OnNoClick()
-    {
-        tribe0Flavor.gameObject.SetActive(true);
-        tribe1Flavor.gameObject.SetActive(true);
-        tribe2Flavor.gameObject.SetActive(true);
-        tribe3Flavor.gameObject.SetActive(true);
-
-        strengthSlider.gameObject.SetActive(true);
-        agilitySlider.gameObject.SetActive(true);
-        trustSlider.gameObject.SetActive(true);
-        survivalSlider.gameObject.SetActive(true);
-        notorietySlider.gameObject.SetActive(true);
-
-        okbutton.gameObject.SetActive(true);
-
-        finalDecisionPanel.gameObject.SetActive(false);
-    }
-
-    private void ResetStats()
-    {
-        tribeType = GlobalsScript.TribeType.Barbarian;
-        strengthStat = 0;
-        agilityStat = 0;
-        trustStat = 0;
-        survivalStat = 0;
-        notorietyStat = 0;
-
-        // Initializing all stats and values to their default states
-        PlayerPrefs.SetString(playerSelectionController.GetCurrentTribeTypeKey(), tribeType.ToString());
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentStrengthStatKey(), strengthStat);
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentAgilityStatKey(), agilityStat);
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentTrustStatKey(), trustStat);
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentSurvivalStatKey(), survivalStat);
-        PlayerPrefs.SetInt(playerSelectionController.GetCurrentNotorietyStatKey(), notorietyStat);
-    }
-
-    public void CreateTribe()
-    {
-        //playerInfo.Players = new PlayerClass();
-
-    }
->>>>>>> 7e24bb51ae559c2879869d4f4797fddbd439940c
 }
